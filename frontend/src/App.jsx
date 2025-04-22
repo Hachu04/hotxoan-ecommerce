895fb3670eb155021007ac9d0854aa1fa837e878
Hachu04 <chuvietha04@gmail.com>
2025-04-21 23:59:02 -0400

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import PlaceOrder from './pages/PlaceOrder'
+import Orders from './pages/Orders'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import SearchBar from './components/SearchBar'
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] 1g:px-[9vw]'>
+      <Navbar />
+      <SearchBar />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/collection' element={<Collection />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/product/:productId' element={<Product />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/place-order' element={<PlaceOrder />} />
+        <Route path='/orders' element={<Orders />} />
+        
+        {/* Catch-all route for 404 Not Found */}
+        <Route path='/404' element={<div>404 Not Found</div>} />
+        <Route path='*' element={<div>404 Not Found</div>} />
+      </Routes>
+
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/BestSeller.jsx
@@ -0,0 +1,37 @@
+import React, { use, useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext';
+import Title from './Title';
+import ProductItem from './ProductItem';
+
+const BestSeller = () => {
+
+    const {products} = useContext(ShopContext);
+    const [bestSeller, setBestSeller] = useState([]);
+
+    useEffect(() => {
+        const bestProducts = products.filter((item)=>(item.bestseller));
+        setBestSeller(bestProducts.slice(0,5));
+    },[]);
+
+  return (
+    <div className='my-10'>
+        <div className='text-center py-8 text-3xl'>
+            <Title text1={'BEST'} text2={'SELLERS'}/>
+            <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+                Explore our best-selling products that have captured the hearts of our customers. These items are not just popular; they are loved for their quality and style. Don't miss out on these favorites!
+            </p>
+        </div>
+
+        {/* Rendering Products */}
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+            {
+                bestSeller.map((item,index) => (
+                    <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price}/>
+                ))
+            }
+        </div>
+    </div>
+  )
+}
+
+export default BestSeller
